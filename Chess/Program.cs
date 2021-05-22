7462a5bcbb8f44507b45f3039dfe2c167b7252ef
Злой Чай, который Салават <abrarov201603@mail.ru>
2021-05-22 14:16:50 +0300

    Добавьте файлы проекта.

---
+++ Chess/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Abraros.S.D 
+  Chess-3
+  15.03.2021
+  */
+
+using System;
+using FigureCore;
+
+namespace chess_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figName = Console.ReadLine();
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            bool isValid = false;
+            Figure figure;
+
+            switch (figName)
+            {
+                case "N":
+                    figure = new Knight(x1, y1);
+                    break;
+                case "Q":
+                    figure = new Queen(x1, y1);
+                    break;
+                case "B":
+                    figure = new Bishop(x1, y1);
+                    break;
+                case "K":
+                    figure = new King(x1, y1);
+                    break;
+                case "R":
+                    figure = new Rook(x1, y1);
+                    break;
+                default:
+                    return;
+            }
+            isValid = figure.Move(x2, y2);
+            Console.WriteLine(isValid ? "YES" : "NO");
+        }
+    }
+}
---
+++ FigureCore/Figure.cs
@@ -0,0 +1,90 @@
+﻿/* Abrarov.S.D 
+  Chess-3
+  15.03.2021
+  */
+
+using System;
+
+namespace FigureCore
+{
+
+    public class Figure
+    {
+        protected int X1;
+        protected int Y1;
+
+        public Figure(int x1, int y1)
+        {
+            X1 = x1;
+            Y1 = y1;
+        }
+
+        public bool Move(int x2, int y2)
+        {
+            if (CanMove(x2, y2))
+            {
+                X1 = x2;
+                Y1 = y2;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public virtual bool CanMove(int x2, int y2)
+        {
+            return false;
+        }
+    }
+
+    public class King : Figure
+    {
+        public King(int x1, int y1) : base(x1, y1) { }
+
+        public override bool CanMove(int x2, int y2)
+        {
+            return (((Math.Abs(X1 - x2) <= 1) && (Math.Abs(y2 - Y1)) <= 1));
+        }
+
+    }
+
+public class Queen : Figure
+    {
+        public Queen(int x1, int y1) : base(x1, y1) { }
+
+        public override bool CanMove(int x2, int y2)
+        {
+            return (X1 == x2 || Y1 == y2) || (Math.Abs(x2 - X1) == (Math.Abs(y2 - Y1)));
+        }
+    }
+
+public class Rook : Figure
+    {
+        public Rook(int x1, int y1) : base(x1, y1) { }
+
+        public override bool CanMove(int x2, int y2)
+        {
+            return ((X1 == x2 || Y1 == y2) & (X1 != x2 || Y1 != y2));
+        }
+    }
+
+public class Bishop : Figure
+    {
+        public Bishop(int x1, int y1) : base(x1, y1) { }
+
+        public override bool CanMove(int x2, int y2)
+        {
+            return (Math.Abs(x2 - X1) == Math.Abs(y2 - Y1));
+        }
+    }
+
+public class Knight : Figure
+    {
+        public Knight(int x1, int y1) : base(x1, y1) { }
+
+        public override bool CanMove(int x2, int y2)
+        {
+            return (((Math.Abs(X1 - x2)) == 2) && (Math.Abs(Y1 - y2) == 1) || ((Math.Abs(X1 - x2)) == 1) && (Math.Abs(Y1 - y2) == 2));
+        }
+    }
+}
